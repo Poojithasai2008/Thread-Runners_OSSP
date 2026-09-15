65fc8641416919a593dec4a4d1758ae54ba6faae
Poojithasai2008 <saipoojitha1984@gmail.com>
2026-09-15 21:23:08 +0530

    Create segmentation.c

---
+++ 2520030103_Practical/Week_5/segmentation.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int *ptr = NULL;
+    int x = 25;
+    ptr = &x;
+    printf("x = %d \n", x);
+    printf("address of x = %u \n", &x);
+    printf("value of ptr = %u \n", ptr);
+    printf("value at ptr = %d \n", *ptr);
+    printf("address of ptr = %u \n", &ptr);
+    return 0;
+}
