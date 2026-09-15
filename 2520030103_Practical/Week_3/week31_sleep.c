b3445e5a851a55fc94472603b0af5b8df8a2a0f4
Poojithasai2008 <saipoojitha1984@gmail.com>
2026-09-15 21:06:40 +0530

    Create week31_sleep.c

---
+++ 2520030103_Practical/Week_3/week31_sleep.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+int main() {
+     int pid;
+     printf("Start of parent\n");
+     pid = fork(); //create child process
+     /*if(pid == 0) { // body of  child process
+         printf(" Start of Child \n");
+         sleep(15); // child process sleep for 15 sec
+         exit(0); // force termination of child without completing child process
+         printf("End of Child \n");
+     }
+     else { // body of  parent process
+         sleep(10); 
+         wait(NULL); 
+       */  printf("End of Parent\n");
+     //+}
+     return 0;
+}
